7f24c4f2795f4280502d20e23da47b61f53cb742
usmanmirCS <47401816+usmanmirCS@users.noreply.github.com>
2020-05-12 13:00:23 -0600

    Session 12

---
+++ March-AM/Assets/Scripts/VR/VRLocomotion.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRLocomotion : MonoBehaviour
+{
+    public Transform m_VRRig;
+    public string m_horizontal;
+    public string m_vertical;
+    public Transform m_director;
+    public LayerMask m_groundLayer;
+    public Transform m_VRHead;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 touchPosition;
+        touchPosition.x = Input.GetAxis(m_horizontal);
+        touchPosition.y = Input.GetAxis(m_vertical);
+        //touchPosition.Normalize(); //If not round Axis
+
+        Vector3 playerRight = touchPosition.x * m_director.right;
+        Vector3 playerForward = touchPosition.y * m_director.forward;
+
+        m_VRRig.position += playerRight;
+        m_VRRig.position += playerForward;
+        m_VRRig.position = new Vector3(m_VRRig.position.x, GetLayerMask(m_VRRig.position.y), m_VRRig.position.z);
+    }
+
+    float GetLayerMask(float y)
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(m_VRHead.position, Vector3.down, out hit, 100f, m_groundLayer))
+        {
+            return hit.point.y;
+        }
+        return y;
+    }
+}
---
+++ March-AM/Assets/Scripts/VR/VRTeleport.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRTeleport : MonoBehaviour
+{
+    public Transform m_VrRig;
+    public string m_buttonName;
+
+    private LineRenderer m_teleportLine;
+    private bool m_validTeleport;
+    private RaycastHit m_hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_teleportLine = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton(m_buttonName))
+        {
+            if (Physics.Raycast(transform.position, transform.forward, out m_hit))
+            {
+                m_teleportLine.enabled = true;
+                m_teleportLine.SetPosition(0, transform.position);
+                m_teleportLine.SetPosition(1, m_hit.point);
+                m_validTeleport = true;
+            }
+            else
+            {
+                m_teleportLine.enabled = false;
+                m_validTeleport = false;
+            }
+        }
+        else if(Input.GetButtonUp(m_buttonName))
+        {
+            m_teleportLine.enabled = false;
+            if(m_validTeleport)
+            {
+                m_VrRig.position = m_hit.point;
+            }
+        }
+    }
+}
